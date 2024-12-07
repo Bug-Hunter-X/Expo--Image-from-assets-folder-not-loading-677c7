e7819a92e786ab6506b68cf6056dec1de79666e9
BugHunter-X <askzenai@gmail.com>
2024-12-07 12:36:07 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,23 @@
+import * as React from 'react';
+import { Text, View, StyleSheet, Image } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Image source={require('./assets/icon.png')} style={styles.image} resizeMode="contain" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  image: {
+    width: 100,
+    height: 100,
+  },
+});
